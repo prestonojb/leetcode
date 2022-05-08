323910947689ed715e7d6c8d4133d24bd47b5407
prestonojb <54185862+prestonojb@users.noreply.github.com>
2022-05-08 22:15:43 +0800

    Time: 293 ms (53.74%), Space: 21.1 MB (82.38%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -46,9 +46,9 @@ class Solution:
             res[i] = res[i-1] * nums[i-1]
         
         # res = [1,1,2,6]
-        postproduct = nums[n-1]
-        for i in range(n-2,-1,-1):
-            res[i] = res[i] * postproduct
+        postproduct = 1
+        for i in range(n-1,-1,-1):
+            res[i] *= postproduct
             postproduct *= nums[i]
         
         return res
