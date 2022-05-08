91bd9b6d36795e5fb6f1e371436b43019ff385cb
prestonojb <54185862+prestonojb@users.noreply.github.com>
2022-05-08 22:08:57 +0800

    Time: 322 ms (42.21%), Space: 21 MB (96.52%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -6,32 +6,49 @@
 # Time complexity: O(N)
 
 
+# class Solution:
+#     def productExceptSelf(self, nums):
+#         n = len(nums)
+#         # Compute pre_product
+        
+#         pre_product = [None] * n
+#         j = 1
+#         for i in range(n):
+#             j *= nums[i]
+#             pre_product[i] = j
+        
+#         # Compute post_product
+#         post_product = [None] * n
+#         k = 1
+#         for i in range(n-1, -1, -1):
+#             k *= nums[i]
+#             post_product[i] = k
+            
+#         result = [None] * n
+        
+#         # Loop to calculate result[i]
+#         for i in range(n):
+#             pre = pre_product[i-1] if i-1>=0 else 1
+#             post = post_product[i+1] if i+1<n else 1
+#             result[i] = pre * post
+            
+#         # Return result
+#         return result 
+
 class Solution:
     def productExceptSelf(self, nums):
         n = len(nums)
-        # Compute pre_product
+        res = [None] * n
         
-        pre_product = [None] * n
-        j = 1
-        for i in range(n):
-            j *= nums[i]
-            pre_product[i] = j
+        # Compute intermediate result
+        res[0] = 1
+        for i in range(1, n):
+            res[i] = res[i-1] * nums[i-1]
         
-        # Compute post_product
-        post_product = [None] * n
-        k = 1
-        for i in range(n-1, -1, -1):
-            k *= nums[i]
-            post_product[i] = k
-            
-        result = [None] * n
+        # res = [1,1,2,6]
+        postproduct = nums[n-1]
+        for i in range(n-2,-1,-1):
+            res[i] = res[i] * postproduct
+            postproduct *= nums[i]
         
-        # Loop to calculate result[i]
-        for i in range(n):
-            pre = pre_product[i-1] if i-1>=0 else 1
-            post = post_product[i+1] if i+1<n else 1
-            result[i] = pre * post
-            
-        # Return result
-        return result 
-            
+        return res
