4befca209ca324570324034f748f003112c63379
prestonojb <54185862+prestonojb@users.noreply.github.com>
2022-05-08 21:40:09 +0800

    Time: 427 ms (14.13%), Space: 23.4 MB (7.68%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -0,0 +1,37 @@
+# Approach: Precompute products
+# nums = [1,2,3,4]
+# pre_product = [1,2,6,24]
+# post_product = [24,24,12,4]
+# res = [pre_product[i-1], post_product[i+1], ...] <- for i in len(nums)
+# Time complexity: O(N)
+
+
+class Solution:
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        # Compute pre_product
+        
+        pre_product = [None] * n
+        j = 1
+        for i in range(n):
+            j *= nums[i]
+            pre_product[i] = j
+        
+        # Compute post_product
+        post_product = [None] * n
+        k = 1
+        for i in range(n-1, -1, -1):
+            k *= nums[i]
+            post_product[i] = k
+            
+        result = [None] * n
+        
+        # Loop to calculate result[i]
+        for i in range(n):
+            pre = pre_product[i-1] if i-1>=0 else 1
+            post = post_product[i+1] if i+1<n else 1
+            result[i] = pre * post
+            
+        # Return result
+        return result 
+            
