0271302cbd862491d0a13fd47a49fee13ef9b346
prestonojb <54185862+prestonojb@users.noreply.github.com>
2022-05-27 18:44:03 +0800

    Time: 125 ms (98.97%), Space: 16.8 MB (20.66%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,11 +1,14 @@
+# 1. Brute force
+# 2. Dictionary 
+# 3. Sorted array
+# 4. XOR (optimized)
+#    x ^ x = 0
+#    For duplicate elements in list, nullify to 0, left is the unduplicated element
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        d = dict()
+        xor = 0
         for num in nums:
-            if d.get(num):
-                del d[num]
-            else:
-                d[num] = 1
-        for key in d.keys():
-            return key
+            xor ^= num
+        return xor
         
