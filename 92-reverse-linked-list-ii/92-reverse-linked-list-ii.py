5cfcd09d773f98d345df18ac9c3fbb1a4ee000fe
Preston Ong <54185862+prestonojb@users.noreply.github.com>
2022-08-08 08:54:11 +0800

    Time: 34 ms (91.18%), Space: 14.1 MB (51.16%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
@@ -5,28 +5,33 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        m = left
-        n = right
-        if m == n:
+        if left == right:
             return head
-
-        dummyNode = ListNode(0)
-        dummyNode.next = head
-        pre = dummyNode
-
-        for i in range(m - 1):
-            pre = pre.next
         
-        # reverse the [m, n] nodes
-        reverse = None
-        cur = pre.next
-        for i in range(n - m + 1):
-            nxt = cur.next
-            cur.next = reverse
-            reverse = cur
-            cur = nxt
+        if left == 1:
+            left_endpoint = None
+            start_of_sorted_portion = head
+        else:
+            left_endpoint = head
+            for _ in range(left-1-1):
+                left_endpoint = left_endpoint.next
+            start_of_sorted_portion = left_endpoint.next
 
-        pre.next.next = cur
-        pre.next = reverse
-
-        return dummyNode.next
+        right_endpoint = head
+        for _ in range(right):
+            right_endpoint = right_endpoint.next
+            
+        prev = None
+        curr = start_of_sorted_portion
+        
+        for _ in range(right - left + 1):
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        
+        if left_endpoint:
+            left_endpoint.next = prev
+        start_of_sorted_portion.next = right_endpoint
+        
+        return head if left_endpoint else prev
