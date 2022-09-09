5a2ad7857ce0f97f553d8e0910446219395c7674
Preston Ong <54185862+prestonojb@users.noreply.github.com>
2022-09-09 14:48:14 +0800

    Time: 95 ms (18.06%), Space: 16.4 MB (10.35%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -9,14 +9,15 @@ class Solution:
         res = [0]
         
         def dfs(root):
-            if not root:
+            if root is None:
                 return -1
-            left = dfs(root.left)
-            right = dfs(root.right)
-            res[0] = max(res[0], 2 + left + right)
             
-            return 1 + max(left, right)
+            leftHeight = dfs(root.left) + 1
+            rightHeight = dfs(root.right) + 1
+            
+            res[0] = max(res[0], leftHeight + rightHeight)
+            
+            return max(leftHeight, rightHeight)
         
         dfs(root)
         return res[0]
-        
