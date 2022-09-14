2c931310e024b8331a7b33e834d05dd93c3b45e7
Preston Ong <54185862+prestonojb@users.noreply.github.com>
2022-09-14 10:41:02 +0800

    Time: 14261 ms (7.29%), Space: 15.1 MB (22.23%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,23 +1,11 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        res = 0
-        pos = 0
-        dest_idx = len(nums) - 1
-        while pos < dest_idx:
-            jump_len = nums[pos]
-            greedy_land_idx = -1
-            for land_idx in range(pos + 1, pos + jump_len + 1):
-                if land_idx == dest_idx:
-                    return res + 1
-                jump_dist = land_idx - pos
-                nxt_jump_len = nums[land_idx]
-                score = jump_dist + nxt_jump_len
-                greedy_land_idx_score = greedy_land_idx - pos + nums[greedy_land_idx]
-                if greedy_land_idx == -1 or score > greedy_land_idx_score:
-                    greedy_land_idx = land_idx
-            pos = greedy_land_idx
-            res += 1
-        return res
+        dp = [float("inf") for _ in range(len(nums))]
+        dp[-1] = 0
         
-                
-                
+        for i in range(len(nums)-2, -1,-1):
+            for k in range(i+1, i + nums[i] + 1):
+                if k < len(nums):
+                    dp[i] = min(dp[i], 1 + dp[k])
+    
+        return dp[0]
