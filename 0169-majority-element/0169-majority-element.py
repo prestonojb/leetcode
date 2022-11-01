2f567a63336d4aaae91b1b5bb1af7c68541fac68
Preston Ong <54185862+prestonojb@users.noreply.github.com>
2022-11-01 11:37:19 +0800

    Time: 372 ms (50.95%), Space: 15.5 MB (85.97%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         countMap = {}
+        threshold = math.ceil(len(nums)/2)
         for n in nums:
           countMap[n] = countMap.get(n, 0) + 1
-          if countMap[n] >= math.ceil(len(nums)/2):
+          if countMap[n] >= threshold:
             return n
